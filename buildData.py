b8c0daf774710af490cdc067e5abb4b88b030103
jrockw <jrockw@umich.edu>
2019-07-17 19:34:18 +0000

    new version of random data test

--- buildData.py
+++ buildData.py
@@ -1,7 +1,7 @@
 import os
 import random as rd
 import shutil
-
+from PIL import Image
 def copytree(src, dst, symlinks=False, ignore=None): 
     for item in os.listdir(src):
         s = os.path.join(src, item)
@@ -10,6 +10,14 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copytree(s,d,symlinks,ignore)
         else: 
             shutil.copy2(s,d)
+
+def copyWhole(src, dst): 
+    img = Image.open(src)
+    if (img.size == 256, 256):
+        shutil.copy2(src, dst)
+    else: 
+        print("Image not copied, size: "+ img.size)
+        print("File name " + src)
 #Missing D2 and B1, these will be reserved for further testing
 folders = {"A1", "A2", "A3", "A4", "A5", "A6", "A7", "B2", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "D1", "D3", "D4", "D5", "D6", "D7", "E1", "E2", "E3", "E4", "E5", "E6", "E7"}
 datashare = "/Datashare/Users/jrockw"
@@ -28,27 +36,27 @@ for core in folders:
     for i in range(202): 
         ind = len(randomFiles[i]) - 4
         num = randomFiles[i][11:ind]
-        if (num != "2025"):
+        if (num != "4900"):
             HEFile = os.path.join(he, randomFiles[i])
-            DAPIFile = os.path.join(DAPI, core + "_DAPI_invrt--"+num+".png")
-            shutil.copy2(HEFile, HEDest)
-            shutil.copy2(DAPIFile, DAPIDest)
+            DAPIFile = os.path.join(DAPI,"DAPI_" + core + "_invrt-"+num+".png")
+            copyWhole(HEFile, HEDest)
+            copyWhole(DAPIFile, DAPIDest)
     for i in range(203,304): 
         ind = len(randomFiles[i]) - 4
         num = randomFiles[i][11:ind]
-        if (num != "2025"):
+        if (num != "4900"):
             HEFile = os.path.join(he, randomFiles[i])
-            DAPIFile = os.path.join(DAPI, core + "_DAPI_invrt--"+num+".png")
-            shutil.copy2(HEFile, HETest)
-            shutil.copy2(DAPIFile, DAPITest)
+            DAPIFile = os.path.join(DAPI, "DAPI_" + core + "_invrt-"+num+".png")
+            copyWhole(HEFile, HEDest)
+            copyWhole(DAPIFile, DAPIDest)
     for i in range(305, 355): 
         ind = len(randomFiles[i]) - 4
         num = randomFiles[i][11:ind]
-        if (num != "2025"):
+        if (num != "4900"):
             HEFile = os.path.join(he, randomFiles[i])
-            DAPIFile = os.path.join(DAPI, core + "_DAPI_invrt--"+num+".png")
-            shutil.copy2(HEFile, HEVal)
-            shutil.copy2(DAPIFile, DAPIVal)
+            DAPIFile = os.path.join(DAPI, "DAPI_" + core + "_invrt-"+num+".png")
+            copyWhole(HEFile, HEDest)
+            copyWhole(DAPIFile, DAPIDest)
         
         
     #TRAIN AND TEST, GET RID OF WHITE? 
